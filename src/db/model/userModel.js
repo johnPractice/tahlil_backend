a96a8b37eb641a0be9012375d386961e75eb6872
Reza Mousavian <rezamosavian1014@gmail.com>
2020-10-20 11:10:31 -0700

    create user model

---
+++ src/db/model/userModel.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const validator = require("validator");
+const constants = require('../../../constants');
+const Schema = mongoose.Schema;
+// create user schema
+const userSchema = new Schema({
+    firstname: {
+        type: String,
+        require: true,
+        minlength: 4,
+    },
+    lastname: {
+        type: String,
+        require: true,
+        minlength: 4,
+    },
+    username: {
+        type: String,
+        unique: true,
+        require: true,
+        minlength: 6
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                return new Error("email not valid");
+            }
+        },
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6,
+    },
+    birthday: {
+        type: Date,
+    },
+    tokens: [{
+        token: {
+            type: String,
+            require: true
+        }
+    }]
+
+}, {
+    autoCreate: true,
+    autoIndex: true,
+    timestamps: true,
+});
+
+
+
+// create the user model
+const User = mongoose.model('User', userSchema);
+module.exports = User;
