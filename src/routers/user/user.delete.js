bfed1fbf68605c6a7f9e80409e225eee2cd308c6
AmiraliPak <a4amirali@hotmail.com>
2020-10-24 19:08:13 +0330

    added user delete api

---
+++ src/routers/user/user.delete.js
@@ -0,0 +1,16 @@
+const auth = require('../../middelware/auth');
+const User = require('../../db/model/userModel');
+const rout = require('express').Router();
+
+//user delete account
+rout.delete('/delete', auth, async (req, res) => {
+    try {
+        const user = req.user;
+
+        await User.remove(user, (err) => {
+            res.status(503).json(err);
+        });
+        res.sendStatus(200);
+
+    } catch (e) { res.status(400).json(e); }
+});
