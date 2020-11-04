cb9a7750ff905675425fdb6742cdc324df193650
Reza Mousavian <rezamosavian1014@gmail.com>
2020-11-04 00:56:09 -0800

    search class by id class

--- src/routers/class/classRouts.js
+++ src/routers/class/classRouts.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const createClass = require('./createClass');
+const searchClass = require('./searchClass');
+
 app.use('/', createClass);
+app.use('/', searchClass);
+
 
 module.exports = app;
