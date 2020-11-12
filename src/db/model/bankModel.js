3c33854cc6676720bc28bcabbca177fd0bfbcc3a
Reza Mousavian <rezamosavian1014@gmail.com>
2020-11-12 12:44:47 -0800

    add answer to bank

--- src/db/model/bankModel.js
+++ src/db/model/bankModel.js
@@ -35,6 +35,7 @@ const bankSchema = Schema({
         required: true,
         enum: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
     },
+    answer: {},
     base: {
         type: String,
         enum: ['10', '11', '12'],
