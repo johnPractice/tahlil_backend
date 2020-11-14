d280f27177cf41c1f5d37bdd84002d1b7f249e36
Reza Mousavian <rezamosavian1014@gmail.com>
2020-11-13 23:42:11 -0800

    add forgot password

--- src/routers/user/userRouts.js
+++ src/routers/user/userRouts.js
@@ -5,11 +5,15 @@ const userProfile = require('./user.profile');
 const userLogout = require('./user.logout');
 const userDelete = require('./user.delete');
 const userClasses = require('./user.Classes');
+const userForgot = require('./forget.password');
+
 
 app.use(userProfile);
 app.use(userLogout);
 app.use(userLogin);
 app.use(userDelete);
 app.use(userClasses);
+app.use(userForgot);
+
 
 module.exports = app;
