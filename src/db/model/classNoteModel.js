6d783b6689b33c0c3078a85c434d6380aa137e65
AmiraliPak <a4amirali@hotmail.com>
2020-11-22 22:36:11 +0330

    Add createClassNote API

--- src/db/model/classNoteModel.js
+++ src/db/model/classNoteModel.js
@@ -5,18 +5,21 @@ const classNoteSchema = Schema({
     title: {
         type: String,
         minLenght: 5,
-        required: true
+        required: [true, 'title is required']
     },
     body: {
         type: String,
         minLenght: 5,
-        required: true
+        maxLength: 120,
+        required: [true, 'body is required']
     },
     creator: {
         type: Schema.Types.ObjectId,
         ref: 'User'
     }
 }, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
     autoCreate: true,
     autoIndex: true,
     timestamps: true
@@ -24,14 +27,17 @@ const classNoteSchema = Schema({
 
 //methods
 classNoteSchema.methods.toJSON = function () {
-    // this refer to clas
-    this.populate('creator', 'firstname lastname avatar').execPopulate();
-
-    const userObject = this.toObject();
-    delete userObject.createdAt;
-    delete userObject.__v;
-
-    return userObject;
+    // this refers to class
+    const creatorJSON = this.creator.toJSON();
+    const object = this.toObject();
+    object.creator = creatorJSON;
+    object.classNoteId = object._id;
+    delete object._id;
+    delete object.id;
+    delete object.updatedAt;
+    delete object.__v;
+    
+    return object;
 };
 
 const classNoteModel = mongoose.model('ClassNote', classNoteSchema);
