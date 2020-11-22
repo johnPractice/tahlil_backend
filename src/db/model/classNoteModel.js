548cfc54880b9137d5d04f857ba50745d7741056
AmiraliPak <a4amirali@hotmail.com>
2020-11-22 18:51:57 +0330

    Add classNoteModel in DB

---
+++ src/db/model/classNoteModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const classNoteSchema = Schema({
+    title: {
+        type: String,
+        minLenght: 5,
+        required: true
+    },
+    body: {
+        type: String,
+        minLenght: 5,
+        required: true
+    },
+    creator: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {
+    autoCreate: true,
+    autoIndex: true,
+    timestamps: true
+});
+
+//methods
+classNoteSchema.methods.toJSON = function () {
+    // this refer to clas
+    this.populate('creator', 'firstname lastname avatar').execPopulate();
+
+    const userObject = this.toObject();
+    delete userObject.createdAt;
+    delete userObject.__v;
+
+    return userObject;
+};
+
+const classNoteModel = mongoose.model('ClassNotes', classNoteSchema);
+
+module.exports = classNoteModel;
