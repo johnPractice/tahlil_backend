1935a01cabb3d6ab033fac67b2d0caa7bce0388c
Reza Mousavian <rezamosavian1014@gmail.com>
2020-11-23 11:00:52 -0800

    add edit member in exam

--- src/routers/exam/examRouts.js
+++ src/routers/exam/examRouts.js
@@ -3,12 +3,17 @@ const examCreate = require('./exam.create');
 const examGet = require('./exam.get');
 const examDelete = require('./exam.delete');
 const examUpdate = require('./exam.update');
+const addMemeber = require('./exam.addUser');
+const editMemeber = require('./exam.editUser');
+
+
 
 const app = express();
 app.use('/', examCreate);
 app.use('/', examGet);
 app.use('/', examDelete);
 app.use('/', examUpdate);
-
+app.use('/', addMemeber);
+app.use('/', editMemeber);
 
 module.exports = app;
