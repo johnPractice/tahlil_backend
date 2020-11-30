df7b4544b80684a097764be4b4e6ea77653b8b4e
Reza Mousavian <rezamosavian1014@gmail.com>
2020-11-29 21:59:48 -0800

    fix limit

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ const initiate = async() => {
 
     // app.use(bodyParser.json());
     // app.use(bodyParser.urlencoded({ extended: false }));
-    // app.use(express.json());
+    app.use(express.json({ limit: "50mb" }));
 
     app.use(bodyParser.json({ limit: "50mb" }));
     app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
