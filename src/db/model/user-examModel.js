5a592f420b43667684e35e0843ea6d2e80bea174
Reza Mousavian <rezamosavian1014@gmail.com>
2020-12-03 06:58:26 -0800

    add base config db model

--- src/db/model/user-examModel.js
+++ src/db/model/user-examModel.js
@@ -6,29 +6,38 @@ const user_examSchema = new Schema({
         ref: "User",
         required: [true, 'کاربر باید مقدار معتبری باشد']
     },
-    exams: [{
-        exam: {
+    exam: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Exam',
+        required: [true, 'ازمون باید مقدار معتبری داشته باشد']
+    },
+    answers: [{
+        question: {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Exam',
-            required: [true, 'ازمون باید مقدار معتبری داشته باشد']
+            ref: 'Question',
+            required: true
         },
-        totalGrade: {
-            type: Number,
+        userAnswer: {
+            type: String,
             default: null
-        },
-        startTime: {
-            type: Date,
-            required: [true, 'زمان شروع آزمون مقدار معتبری یاید داشته باشد']
-        },
-        //TODO: at pre save we should handel it
-        endTime: {
-            type: Date,
-        },
-        startDate: {
-            type: Date,
-            required: [true, 'تاریخ شروع آزمون مقدار معتبری یاید داشته باشد']
         }
-    }]
+    }],
+    totalGrade: {
+        type: Number,
+        default: null
+    },
+    startTime: {
+        type: Date,
+        required: [true, 'زمان شروع آزمون مقدار معتبری یاید داشته باشد']
+    },
+    //TODO: at pre save we should handel it
+    endTime: {
+        type: Date,
+    },
+    startDate: {
+        type: Date,
+        required: [true, 'تاریخ شروع آزمون مقدار معتبری یاید داشته باشد']
+    }
 });
 
 const user_examModel = mongoose.model('UserExam', user_examSchema);
