6421a53bf6c3acb82cb34b119431fb60eae0151b
AmiraliPak <a4amirali@hotmail.com>
2020-12-06 20:39:33 +0330

    Edit question answer

--- src/routers/exam/uploadFilwAnswerExam.js
+++ src/routers/exam/uploadFilwAnswerExam.js
@@ -1,15 +1,24 @@
 const rout = require('express').Router();
 const auth = require('../../middelware/auth');
 const uploadAnswer = require('../../middelware/exam/fileUploadingExam');
-rout.post('/answer', auth, uploadAnswer.single('answer'), async(req, res) => {
+const checkExamId = require('../../middelware/exam/checkExamId');
+const checkQuestionId = require('../../middelware/exam/checkQuestionId');
+const checkClassAccess = require('../../middelware/class/checkClassAccess');
+const checkExamTime = require('../../middelware/exam/checkExamTime');
+
+rout.post('/:examId/questions/:questionId', auth, checkExamId, checkClassAccess, checkExamTime, checkQuestionId, uploadAnswer.single('answer'), async(req, res) => {
     try {
+        ///////////////////////////////////////////////////////////////
+        const { user, Class, exam, question, user_examEndTime } = req;
+        ///////////////////////////////////////////////////////////////
+
         const filePath = (req.fileName);
         // TODO: check exam and question to save answer path
         if (!filePath) {
             res.status(400).json({ "error": "مشکلی رخ داده است" });
             return;
         }
-        res.status(200).json({ "message": "ok", filePath });
+        res.status(200).json({ "message": "ok", filePath, user_examEndTime });
     } catch (e) {
         res.json(e);
     }
