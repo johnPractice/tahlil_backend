5a6c3b3bcd376e01e08e5350e77d3ca66d86628a
Reza Mousavian <rezamosavian1014@gmail.com>
2020-12-08 23:57:25 -0800

    add download file for app

--- src/routers/publicApi/publicRouts.js
+++ src/routers/publicApi/publicRouts.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const questionCategory = require('./questionCategory');
 const time = require('./getTime');
+const downloadApp = require('./downloadApp');
 app.use('/', questionCategory);
 app.use('/', time);
+app.use('/', downloadApp);
 module.exports = app;
