749bea5410aa75f833ab93371dc48db4f51e25d4
AmiraliPak <a4amirali@hotmail.com>
2020-12-12 23:41:34 +0330

    Fix check answer and question type Fix bugs Fix swagger

--- src/routers/exam/uploadFilwAnswerExam.js
+++ src/routers/exam/uploadFilwAnswerExam.js
@@ -6,19 +6,33 @@ const checkExamId = require('../../middelware/exam/checkExamId');
 const checkClassAccess = require('../../middelware/class/checkClassAccess');
 const checkExamTime = require('../../middelware/exam/checkExamTime');
 const checkQuestionIndex = require('../../middelware/exam/checkQuestionIndex');
+const questionModel = require('../../db/model/questionModel');
 
 
 rout.post('/:examId/questions/:questionIndex/answer', auth, checkExamId, checkClassAccess, checkExamTime, checkQuestionIndex, uploadAnswer.single('answer'), async(req, res) => {
     try {
+        const { questionObj, user_exam, user_examEndTime } = req;
+
+        const questionType = questionObj.question.type;
+        const questionOptionsLength = questionObj.question.options.length;
         const answerFile = req.fileName;
-        const answerText =  req.query.answer;
+        let answerText =  req.query.answer;
         // TODO: check exam and question to save answer path
         if (!answerFile && !answerText) {
             res.status(400).json({ "error": "مشکلی رخ داده است" });
             return;
         }
-
-        const { questionObj, user_exam, user_examEndTime } = req;
+        else if (answerFile && questionType != 'LONGANSWER') {
+            //TODO: delete file
+            throw { message: "File upload is for LONGANSWER questions only", code: 400 };
+        }
+        else if (answerText) {
+            answerText = questionModel.validateAnswer({
+                answer: answerText,
+                questionType,
+                questionOptionsLength
+            });
+        }
 
         const foundAnswer = user_exam.answers.find(answer => answer.questionIndex == questionObj.index);
         if (!foundAnswer) {
@@ -31,18 +45,30 @@ rout.post('/:examId/questions/:questionIndex/answer', auth, checkExamId, checkCl
             if (answerFile)
                 newAnswer.answerFile = answerFile;
             user_exam.answers.push(newAnswer);
+            var response = {
+                answerText: newAnswer.answerText,
+                answerFile: newAnswer.answerFile,
+                user_examEndTime
+            };
 
         } else {
             if (answerText)
                 foundAnswer.answerText = answerText;
             if (answerFile)
                 foundAnswer.answerFile = answerFile;
+            var response = {
+                answerText: foundAnswer.answerText,
+                answerFile: foundAnswer.answerFile,
+                user_examEndTime
+            };
         }
         await user_exam.save();
-        res.status(200).json({ answerText: foundAnswer.answerText, answerFile: foundAnswer.answerFile, user_examEndTime });
+        res.status(200).json(response);
 
-    } catch (e) {
-        res.json(e);
+    } catch (err) {
+        if (!err.code || err.code >= 600)
+            err.code = 503;
+        res.status(err.code).json({ error: err.message });
     }
 });
 rout.delete('/:examId/questions/:questionIndex/answer', auth, checkExamId, checkClassAccess, checkExamTime, checkQuestionIndex, async (req, res) => {
