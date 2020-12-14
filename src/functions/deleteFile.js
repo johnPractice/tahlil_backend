83d350421cf9843972af7b0969d642935ae1e174
AmiraliPak <a4amirali@hotmail.com>
2020-12-14 18:25:44 +0330

    Fix delete answer file Fix deleteFile function

--- src/functions/deleteFile.js
+++ src/functions/deleteFile.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const deleteFile = (pathFile) => {
-    pathFile = pathFile.replace('/', '\\');
+    pathFile = pathFile.split("//").join("/");
     fs.stat(pathFile, function(err, stats) {
         if (err) throw new Error(err);
         fs.unlink(pathFile, function(err) {
